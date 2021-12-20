024cbee01301f38c9c5a4e402f6997eefec2a566
chirag-9121 <chirag9121.cg@gmail.com>
2021-12-20 20:55:32 +0530

    account page routing

--- Account/views.py
+++ Account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,7 +7,7 @@ def signup_view(request):
     return HttpResponse("SIGNUP")
 
 def login_view(request):
-    return HttpResponse("LOGIN")
+    return render(request,"Account/login.html")
 
 def logout_view(request):
     return HttpResponse("LOGOUT")
