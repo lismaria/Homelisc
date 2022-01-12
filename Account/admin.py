25f6aa597a2cabfb3f1773dbae5e34ffd7e7de5f
lismaria <lismariasaju@gmail.com>
2022-01-12 23:25:14 +0530

    custom user model created

--- Account/admin.py
+++ Account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# from django.conf import settings    # alternate for: from Account.models import User
+# User = settings.AUTH_USER_MODEL
+
+from Account.models import User
+admin.site.register(User)
