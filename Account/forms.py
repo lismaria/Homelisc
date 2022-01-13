f1ca8d58be460b4001dc0e950d77be5ba214fa28
chirag-9121 <chirag9121.cg@gmail.com>
2022-01-13 19:11:21 +0530

    added placeholders and removed labels from signup input

--- Account/forms.py
+++ Account/forms.py
@@ -4,6 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from Account.models import User
 
 class RegistrationForm(forms.ModelForm):
+    name = forms.CharField(label='',max_length=32,widget=forms.TextInput(attrs={'placeholder':'Full Name'}))
+    email= forms.EmailField(label='',max_length=60,widget=forms.EmailInput(attrs={'placeholder':'Email ID'}))
+    password= forms.CharField(label='',max_length=50,widget=forms.PasswordInput(attrs={'placeholder':'Choose a password'}))
+
     class Meta:
         model = User
         fields = ('name','email','password')
