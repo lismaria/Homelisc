926a0c8e73448462aa1613ff621b1daf9da835b4
lismaria <lismariasaju@gmail.com>
2022-01-21 22:09:40 +0530

    Vendor-side URLS

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = "vendor"
 
 urlpatterns = [
-    path('', views.shop_view, name='home'),
-    path('review', views.review_view, name="review"),
-    path('menu', views.menu_view, name='menu'),
+    path('', views.home, name='home'),
+    path('<slug:id>/<slug:slug>/menu', views.menu_view, name='menu'),
+    path('<slug:id>/<slug:slug>/review', views.review_view, name="review"),
     path('register-shop',views.register_shop,name="register-shop"),
-    # path('shop', views.menu_view, name='shop'),
-    re_path('shop/'+r'(?P<shop_idd>[01])',views.shop,name="shop")
-    # re_path(r'[0-9]+\/(?P<shop_slug>[\w-]+)',views.shop,name="shop")
+    path('<slug:id>/<slug:slug>',views.shop_view,name="shop")
 ]
+
+# path('shop', views.menu_view, name='shop'),
+# re_path('shop/'+r'(?P<shop_idd>[01])',views.shop,name="shop")
+# re_path(r'[0-9]+\/(?P<shop_slug>[\w-]+)',views.shop,name="shop")
--- Vendor/views.py
+++ Vendor/views.py
@@ -3,43 +3,33 @@ from Vendor.models import Shop
 
 # Create your views here.
 
-def check_vendor_details(request,render_template):
+def check_vendor_details(request,render_template,id):
     if not request.user.is_authenticated:
         return redirect("home")
     elif (request.user.is_authenticated and request.user.is_vendor == True):
-        shopCount = Shop.objects.filter(shop_owner_id=request.user.id).count()
+        shopInfo = Shop.objects.filter(id=id)
+        shopCount = shopInfo.count()
         if shopCount == 0:
             return render(request,'Vendor/perks.html')
         else:
-            return render(request,"Vendor/"+render_template+".html") #*
+            return render(request,"Vendor/"+render_template+".html",{'shopInfo':shopInfo}) #*
     else:
         return redirect("home")
 
-def shop_view(request):
+def home(request):
     # context=[]
     if not request.user.is_authenticated:
         return render(request,'home.html',{'food':'chocolates'})
     elif (request.user.is_authenticated and request.user.is_vendor == True):
-        shopCount = Shop.objects.filter(shop_owner_id=request.user.id).count()
         shopInfo = Shop.objects.filter(shop_owner_id=request.user.id)
+        shopCount = shopInfo.count()
         print(shopInfo)
-        # context['shopInfo'] = shopInfo
-        # for i in shopInfo:
-        #     print("I",i.id)
         if shopCount == 0:
             return render(request,'Vendor/perks.html')
         else:
             return render(request,'Vendor/home.html',{'shopInfo':shopInfo})
     else:
         return render(request,'home.html',{'food':'chocolates'})
-
-def menu_view(request):
-    menu_view = check_vendor_details(request,"menu")
-    return menu_view
-
-def review_view(request):
-    reviews_view = check_vendor_details(request,"review")
-    return reviews_view
 
 def register_shop(request):
     if not request.user.is_authenticated:
@@ -53,5 +43,14 @@ def register_shop(request):
     else:
         return redirect("home")
 
-def shop(request,shop_idd):
-    return render(request,"Vendor/shop.html")
+def shop_view(request,id,slug):
+    shopInfo = Shop.objects.filter(id=id)
+    return render(request,"Vendor/shop.html",{'shopInfo':shopInfo})
+
+def menu_view(request,id,slug):
+    menu_view = check_vendor_details(request,"menu",id)
+    return menu_view
+
+def review_view(request,id,slug):
+    reviews_view = check_vendor_details(request,"review",id)
+    return reviews_view
